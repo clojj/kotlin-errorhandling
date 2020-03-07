62df6342ae8db35fe767ff1ebaeea73d870104e4
clojj <jwin1968@gmail.com>
2020-03-07 17:52:25 +0100

    cleanup

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -36,27 +36,6 @@ class Person private constructor(val id: Int, val firstName: String, val lastNam
     override fun toString(): String {
         return "Person(id=$id, firstName='$firstName', lastName='$lastName')"
     }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Person
-
-        if (id != other.id) return false
-        if (firstName != other.firstName) return false
-        if (lastName != other.lastName) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id
-        result = 31 * result + firstName.hashCode()
-        result = 31 * result + lastName.hashCode()
-        return result
-    }
-
 }
 
 object Validations {
